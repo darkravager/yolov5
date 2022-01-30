3142a3470f0bfa68f9ba97c6ab9083c313f2c12b
darkravager <88234632+darkravager@users.noreply.github.com>
2022-01-30 14:27:30 +0530

    Update augmentations.py

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -12,7 +12,31 @@ import numpy as np
 from utils.general import LOGGER, check_version, colorstr, resample_segments, segment2box
 from utils.metrics import bbox_ioa
 
+class Val_Albumentations:
+    # YOLOv5 Albumentations class (optional, only used if package is installed)
+    def __init__(self):
+        self.transform = None
+        try:
+            import albumentations as A
+            check_version(A.__version__, '1.0.3', hard=True)  # version requirement
 
+            self.transform = A.Compose([
+                                        A.CLAHE(clip_limit=(4,4),always_apply=True,p=1.0),
+                                        A.RandomBrightness(limit = (0.05,0.05),always_apply=True,p=1.0)],
+                                       bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']),p=1.0)
+
+            LOGGER.info(colorstr('albumentations: ') + ', '.join(f'{x}' for x in self.transform.transforms if x.p))
+        except ImportError:  # package not installed, skip
+            pass
+        except Exception as e:
+            LOGGER.info(colorstr('albumentations: ') + f'{e}')
+
+    def __call__(self, im, labels, p=1.0):
+        if self.transform and random.random() < p:
+            new = self.transform(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0])  # transformed
+            im, labels = new['image'], np.array([[c, *b] for c, b in zip(new['class_labels'], new['bboxes'])])
+        return im, labels
+    
 class Albumentations:
     # YOLOv5 Albumentations class (optional, only used if package is installed)
     def __init__(self):
@@ -22,14 +46,19 @@ class Albumentations:
             check_version(A.__version__, '1.0.3', hard=True)  # version requirement
 
             self.transform = A.Compose([
-                A.Blur(p=0.01),
-                A.MedianBlur(p=0.01),
-                A.ToGray(p=0.01),
-                A.CLAHE(p=0.01),
-                A.RandomBrightnessContrast(p=0.0),
-                A.RandomGamma(p=0.0),
-                A.ImageCompression(quality_lower=75, p=0.0)],
-                bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+                                        A.CLAHE(clip_limit=(4,4),always_apply=True,p=1.0),
+                                        A.RandomBrightness(limit = (0.05,0.05),always_apply=True,p=1.0),
+                                        A.OneOf([
+                                            A.IAAAdditiveGaussianNoise(),
+                                            A.GaussNoise(),
+                                        ], p=0.2),
+                                        A.OneOf([
+                                            A.MotionBlur(p=0.2),
+                                            A.MedianBlur(blur_limit=3, p=0.1),
+                                            A.Blur(blur_limit=3, p=0.1),
+                                        ], p=0.2),
+                                        A.Rotate(limit=30)],
+                                       bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']),p=1.0)
 
             LOGGER.info(colorstr('albumentations: ') + ', '.join(f'{x}' for x in self.transform.transforms if x.p))
         except ImportError:  # package not installed, skip
